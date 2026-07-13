a0cb5922edce30580836917af7f641948ebe22df
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-13 23:06:17 +0530

    Completed Lesson 1 - Hello World

---
+++ 01_Hello.py
@@ -0,0 +1,20 @@
+# Hello world program : To print "Hello World" on terminal
+
+print("Hello World!!!")
+print("Hello World!!!")
+print("Hello World!!!")
+print("Hello World!!!")
+print("Hello World!!!")
+print("Hello World!!!")
+print("Hello World!!!")
+
+# Comments in python
+
+# Single line comment
+
+"""
+This 
+is
+Multi-line
+comment!!
+"""
