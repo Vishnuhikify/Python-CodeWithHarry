592f5308166c1b1a99483b043538cbc539f763e0
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-13 23:32:16 +0530

    Completed Lesson 03 - Typecasting

---
+++ Code-With-Harry/03_Typecasting.py
@@ -0,0 +1,17 @@
+# Typecasting in python
+
+# Converting from string to an integer
+a = '10'
+sum = int(a) + 20
+print(sum)
+
+# Converting from number to string
+a = 20
+total = str(a) + "Amith"
+print(total)
+
+# Converting to float
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
