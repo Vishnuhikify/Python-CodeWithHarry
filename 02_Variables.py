df353e63494aa99b6f025db05ba4881a6c820385
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-13 23:22:39 +0530

    Completed Lesson 02 - Variables

---
+++ 02_Variables.py
@@ -0,0 +1,32 @@
+# Variables in Python
+
+# Numbers
+a = 10 # integer
+print(a)
+
+a = 10.5 # float
+print(a)
+
+# Boolean
+c = True
+print(c)
+
+# String
+d = "Amith"
+print(d)
+
+# None
+e = None
+print(e)
+
+# _______________________________________________________________________________________
+
+# There are different types of data types in Python. Some built-in Python data types are:
+
+# Numeric data types: int, float, complex
+# String data types: str
+# Sequence types: list, tuple, range
+# Binary types: bytes, bytearray, memoryview
+# Mapping data type: dict
+# Boolean type: bool
+# Set data types: set, frozenset
