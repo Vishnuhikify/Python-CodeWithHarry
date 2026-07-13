cd6ff5a892ebb923c500fb935c35f93b570e70fb
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-13 23:40:23 +0530

    Completed Lesson 04 - Operators

---
+++ Code-With-Harry/04_Operators.py
@@ -0,0 +1,47 @@
+# Operators in Python
+
+num1 = 10
+num2 = 10
+
+# 1) Arithmetic operators
+sum = num1 + num2
+print("Addition : ", sum)
+sub = num1 - num2
+print("Subtraction : ", sub)
+mul = num1 * num2
+print("Multiplication : ", mul)
+div = num1 / num2
+print("Division : ", div)
+modulo = num1 % num2
+print("Remainder : ", modulo)
+pow = num1 ** 3
+print("Power : ", pow)
+div2 = num1 // num2
+print("Division without floating value : ", div2)
+
+# Assignment operator 
+a = 2
+print(a)
+a += 2
+print(a)
+a -= 2
+print(a)
+a /= 2
+print(a)
+
+# Comparission Operator
+x = 10
+y = 20
+z = 30
+print(x > y)  
+print(x < y)  
+print(x >= y)  
+print(x <= y)
+print(x != y)
+print(x == y)  
+
+# Logical operator
+print(x==z and x < y)
+print(x==z or x < y)
+print(not(True))
+print(not(False))
