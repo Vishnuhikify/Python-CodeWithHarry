a4780b775fa6501fbf59a0f3dac8d1ade7083eb2
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-14 20:33:14 +0530

    Completed Lesson 05 - User Input

---
+++ Code-With-Harry/05_UserInput.py
@@ -0,0 +1,7 @@
+# name = input("Enter your name : ")
+# print("Hello, " + name)
+
+# Write a python program to accept number from user and add 6 to it
+number = input("Enter a number : ")
+print(int(number) + 6)
+print(type(number))
