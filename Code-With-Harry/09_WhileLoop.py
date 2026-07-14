d9fc3db3ce196b7346af5b7878c36f1aa5d4882a
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-14 21:50:52 +0530

    Completed Lesson 09 While Loops in Python

---
+++ Code-With-Harry/09_WhileLoop.py
@@ -0,0 +1,15 @@
+# While loop
+
+# Example 1 : 
+i = 1
+while(i <= 5):
+    print(i)
+    i += 1
+
+# Example 2 : 
+while(True):
+    num = int(input("Enter a number : "))
+    print(num)
+    if(num == 0):
+        print("Program has finished it's execution.")
+        break
