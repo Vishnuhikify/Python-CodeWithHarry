8402d45c918a1f33e8cf475a62d1eb30ffce1681
Vishnu Prasad <javagalvishnu@gmail.com>
2026-07-17 22:41:47 +0530

    Completed Lesson 11 F-Strings

---
+++ Code-With-Harry/11_fStrings.py
@@ -0,0 +1,10 @@
+# Without F-String
+
+name: str = "Vishnu"
+age: int = 20
+
+print("My name is " + name + " and I am " + str(age) + " years old!!") # More efforts in writing
+
+# Using F-String
+print(f"My name is {name} and I'm {age} years old!!!")
+print(F"My name is {name} and I'm {age} years old!!!")
