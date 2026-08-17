7d339c356884f2dac3ed28bb3ed7b63b5ec73cfe
Vishnu Prasad <javagalvishnu@gmail.com>
2026-08-17 22:58:26 +0530

    Completed Lesson 23 - Importing Modules

---
+++ Code-With-Harry/23_Imports.py
@@ -0,0 +1,10 @@
+import math;
+# import math as m;
+
+# Importing specific method from Math
+from math import sqrt, tan;
+
+a: int = 25
+print(f'Square root of a is {sqrt(a)}')
+print(f'a square is {math.pow(a, 2)}')
+print(tan(2))
